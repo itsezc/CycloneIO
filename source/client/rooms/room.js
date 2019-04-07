8c6c787debd08e379d69e9538b26b2d0c71af833
LevitationX <jasonolimpio3@gmail.com>
2019-04-08 00:07:10 +0200

    Fix canvas tiles

--- source/client/rooms/room.js
+++ source/client/rooms/room.js
@@ -11,7 +11,8 @@ class Room {
   create() {
     this.socket.emit(Constants.common.actions.room.NEW_ROOM, this.id, [
       [0, 0],
-      [0, 0]
+      [0, 0],
+      [0, 0, 0, 0]
     ], {
       x: 0,
       y: 0
@@ -29,28 +30,24 @@ class Room {
         var x = (i * 32) - (j * 32)
         var y = ((i * 32) + (j * 32)) / 2
 
-        this.rooms[id] = this.scene.add.image(x, y, Constants.client.assets.TILE).setOrigin(-1)
+        this.drawTile(x, y)
 
-        /*
-        This is for testing purposes,
-        do not use this for your hotel.
-        */
-        this.addTile(x, y)
+        this.rooms[id] = this.scene.add.image(x, y, 'tile').setOrigin(-2)
       }
     }
   }
 
-  addTile(x, y) {
+  drawTile(x, y) {
     const width = 64
     const height = 32
-    const thick = 7.5
+    const thick = 7 // default
 
     var tile = this.scene.add.graphics()
 
-    tile.beginPath()
-
+    tile.lineStyle(0.5, 0x8E8E5E)
     tile.fillStyle(0x989865)
-    tile.lineStyle(1, 0x8E8E5E)
+
+    tile.beginPath()
 
     tile.moveTo(x, y)
     tile.lineTo(x - width / 2, y + height / 2)
@@ -58,39 +55,38 @@ class Room {
     tile.lineTo(x + width / 2, y + height / 2)
     tile.lineTo(x, y)
 
-    tile.strokePath()
     tile.fillPath()
-    tile.closePath()
+    tile.strokePath()
 
     if (thick > 0) {
+      tile.fillStyle(0x838357)
+      tile.lineStyle(1, 0x7A7A51)
+
       tile.beginPath()
+
+      tile.moveTo(x - width / 2, y + height / 2)
+      tile.lineTo(x - width / 2, y + height / 2 + thick)
+      tile.lineTo(x, y + height + thick)
+      tile.lineTo(x, y + height)
+
+      tile.fillPath()
+      tile.strokePath()
 
       tile.fillStyle(0x6F6F49)
       tile.lineStyle(1, 0x676744)
+
+      tile.beginPath()
 
       tile.moveTo(x + width / 2, y + height / 2)
       tile.lineTo(x + width / 2, y + height / 2 + thick)
       tile.lineTo(x, y + height + thick)
       tile.lineTo(x, y + height)
 
-      tile.strokePath()
       tile.fillPath()
-      tile.closePath()
-
-      tile.beginPath()
-
-      tile.fillStyle(0x838357)
-      tile.lineStyle(1, 0x676744)
-
-      tile.moveTo(x, y + height)
-      tile.lineTo(x, y + height + thick)
-      tile.lineTo(x - width / 2, y + height / 2 + thick)
-      tile.lineTo(x - width / 2, y + height / 2)
-
       tile.strokePath()
-      tile.fillPath()
-      tile.closePath()
     }
+
+    tile.closePath()
   }
 }
 
