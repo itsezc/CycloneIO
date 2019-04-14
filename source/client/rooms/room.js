0e110c443eafcb60208a9c2c0be8ec7fd708f331
LevitationX <jasonolimpio3@gmail.com>
2019-04-14 22:00:44 +0200

    Added walls

--- source/client/rooms/room.js
+++ source/client/rooms/room.js
@@ -12,16 +12,13 @@ export default class Room {
 
 		this.socket.emit(Constants.common.actions.room.NEW_ROOM, this.id,
 			[
-				[1, 1, 1, 1, 0, 0],
-				[1, 1, 1, 1, 1, 0, [1, 2], 0, 0, [1, 3]]
+				[1]
 			]
 		)
 
 		this.socket.on(Constants.common.actions.room.NEW_TILE, (x, y, z, thickness, leftBorder, bottomBorder) => {
 			var tile = this.drawTile(x, y - z, thickness, leftBorder, bottomBorder)
 			var hover
-
-			console.log(z)
 
 			tile.on(Constants.client.events.MOUSE_HOVER, () => {
 				hover = this.scene.add.image(x, y - z, Constants.client.assets.TILE_HOVER).setOrigin(0.5, 0.1)
@@ -30,11 +27,13 @@ export default class Room {
 			tile.on(Constants.client.events.MOUSE_OUT, () => {
 				hover.destroy()
 			})
+
+			this.drawWall(x, y, 120, 7.5, thickness, true, true)
 		})
 	}
 
 	drawTile(x, y, thickness, leftBorder, bottomBorder) {
-		const width = 64
+		const width = 32
 		const height = 32
 
 		const vertices = {
@@ -43,7 +42,7 @@ export default class Room {
 				y: y
 			},
 			top: {
-				x: x - width / 2,
+				x: x - width,
 				y: y + height / 2
 			},
 			bottom: {
@@ -51,7 +50,7 @@ export default class Room {
 				y: y + height
 			},
 			right: {
-				x: x + width / 2,
+				x: x + width,
 				y: y + height / 2
 			}
 		}
@@ -80,8 +79,9 @@ export default class Room {
 
 		tile.fillPath()
 		tile.strokePath()
-		
+
 		if (leftBorder && thickness > 0) {
+
 			tile.lineStyle(1, 0x7A7A51)
 			tile.fillStyle(0x838357)
 
@@ -97,6 +97,7 @@ export default class Room {
 		}
 
 		if (bottomBorder && thickness > 0) {
+
 			tile.fillStyle(0x6F6F49)
 			tile.lineStyle(1, 0x676744)
 
@@ -112,5 +113,108 @@ export default class Room {
 		}
 
 		return tile
+	}
+
+	drawWall(x, y, height, thickness, depth, rightBorder, topBorder) {
+		const width = 32
+
+		var wall = this.scene.add.graphics()
+		var vertices
+		var hitArea
+
+		if (rightBorder) {
+
+			vertices = {
+				left: {
+					x: x,
+					y: y
+				},
+				top: {
+					x: x,
+					y: y - height
+				},
+				right: {
+					x: x + width,
+					y: y + width / 2 - height
+				},
+				bottom: {
+					x: x + width,
+					y: y + width / 2
+				}
+			}
+
+			hitArea = new Phaser.Geom.Polygon([
+				vertices.left.x, vertices.left.y,
+				vertices.top.x, vertices.top.y,
+				vertices.right.x, vertices.right.y,
+				vertices.bottom.x, vertices.bottom.y,
+			])
+
+			wall.lineStyle(1, 0xB6B8C7)
+			wall.fillStyle(0xB6B8C7)
+
+			wall.beginPath()
+
+			wall.moveTo(vertices.left.x, vertices.left.y)
+
+			hitArea.points.forEach((point) => {
+				wall.lineTo(point.x, point.y)
+			})
+
+			wall.fillPath()
+			wall.strokePath()
+		}
+
+		if (topBorder) {
+
+			vertices = {
+				right: {
+					x: x,
+					y: y
+				},
+				top: {
+					x: x,
+					y: y - height
+				},
+				left: {
+					x: x - width,
+					y: (y + width / 2) - height
+				},
+				bottom: {
+					x: x - width,
+					y: y + width / 2
+				}
+			}
+
+			hitArea = new Phaser.Geom.Polygon([
+				vertices.right.x, vertices.right.y,
+				vertices.top.x, vertices.top.y,
+			    vertices.left.x, vertices.left.y,
+				vertices.bottom.x, vertices.bottom.y,
+			])
+
+			wall.lineStyle(1, 0x90929E)
+			wall.fillStyle(0x90929E)
+
+			wall.beginPath()
+
+			wall.moveTo(vertices.right.x, vertices.right.y)
+
+			hitArea.points.forEach((point) => {
+				wall.lineTo(point.x, point.y)
+			})
+
+			wall.fillPath()
+			wall.strokePath()
+
+		}
+
+		wall.setInteractive(hitArea, Phaser.Geom.Polygon.Contains)
+
+		wall.on('pointerdown', () => {
+			console.log('Clicked wall')
+		})
+
+		return wall
 	}
 }
