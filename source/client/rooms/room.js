33d77b32187660074879888557e36b3923184104
Chiru B <chirub@foretag.co>
2019-04-14 17:45:32 +0100

    Room map trial for Height

--- source/client/rooms/room.js
+++ source/client/rooms/room.js
@@ -12,19 +12,19 @@ export default class Room {
 
 		this.socket.emit(Constants.common.actions.room.NEW_ROOM, this.id,
 			[
-				[1, 0, 1],
-				[1, [1, 1]]
+				[1, 1, 1, 1, 0, 0],
+				[1, 1, 1, 1, 1, 0, [1, 2], 0, 0, [1, 3]]
 			]
 		)
 
 		this.socket.on(Constants.common.actions.room.NEW_TILE, (x, y, z, thickness, leftBorder, bottomBorder) => {
-			var tile = this.drawTile(x, y * z, thickness, leftBorder, bottomBorder)
+			var tile = this.drawTile(x, y * (z || 1), thickness, leftBorder, bottomBorder)
 			var hover
 
 			console.log(z)
 
 			tile.on(Constants.client.events.MOUSE_HOVER, () => {
-				hover = this.scene.add.image(x, y * z, Constants.client.assets.TILE_HOVER).setOrigin(0.5, 0.1)
+				hover = this.scene.add.image(x, y * (z || 1), Constants.client.assets.TILE_HOVER).setOrigin(0.5, 0.1)
 			})
 
 			tile.on(Constants.client.events.MOUSE_OUT, () => {
@@ -80,36 +80,36 @@ export default class Room {
 
 		tile.fillPath()
 		tile.strokePath()
-
-		if (leftBorder && thickness > 0) {
-			tile.lineStyle(1, 0x7A7A51)
-			tile.fillStyle(0x838357)
-
-			tile.beginPath()
-
-			tile.moveTo(vertices.top.x, vertices.top.y)
-			tile.lineTo(vertices.top.x, vertices.top.y + thickness)
-			tile.lineTo(vertices.bottom.x, vertices.bottom.y + thickness)
-			tile.lineTo(vertices.bottom.x, vertices.bottom.y)
-
-			tile.fillPath()
-			tile.strokePath()
-		}
-
-		if (bottomBorder && thickness > 0) {
-			tile.fillStyle(0x6F6F49)
-			tile.lineStyle(1, 0x676744)
-
-			tile.beginPath()
-
-			tile.moveTo(vertices.right.x, vertices.right.y)
-			tile.lineTo(vertices.right.x, vertices.right.y + thickness)
-			tile.lineTo(vertices.bottom.x, vertices.bottom.y + thickness)
-			tile.lineTo(vertices.bottom.x, vertices.bottom.y)
-
-			tile.fillPath()
-			tile.strokePath()
-		}
+		//
+		// if (leftBorder && thickness > 0) {
+		// 	tile.lineStyle(1, 0x7A7A51)
+		// 	tile.fillStyle(0x838357)
+		//
+		// 	tile.beginPath()
+		//
+		// 	tile.moveTo(vertices.top.x, vertices.top.y)
+		// 	tile.lineTo(vertices.top.x, vertices.top.y + thickness)
+		// 	tile.lineTo(vertices.bottom.x, vertices.bottom.y + thickness)
+		// 	tile.lineTo(vertices.bottom.x, vertices.bottom.y)
+		//
+		// 	tile.fillPath()
+		// 	tile.strokePath()
+		// }
+		//
+		// if (bottomBorder && thickness > 0) {
+		// 	tile.fillStyle(0x6F6F49)
+		// 	tile.lineStyle(1, 0x676744)
+		//
+		// 	tile.beginPath()
+		//
+		// 	tile.moveTo(vertices.right.x, vertices.right.y)
+		// 	tile.lineTo(vertices.right.x, vertices.right.y + thickness)
+		// 	tile.lineTo(vertices.bottom.x, vertices.bottom.y + thickness)
+		// 	tile.lineTo(vertices.bottom.x, vertices.bottom.y)
+		//
+		// 	tile.fillPath()
+		// 	tile.strokePath()
+		// }
 
 		return tile
 	}
