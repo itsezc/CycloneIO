316cc2e40afba20cdcfb1608e93b7c2bdab16bef
Chiru B <chirub@foretag.co>
2019-04-15 14:41:19 +0100

    R63B UI Setup

---
+++ resources/ui/r63b/states/client/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+//import './client.styl'
+
+export default class Client extends Component {
+
+	constructor(props) {
+		super(props)
+
+		this.state = {}
+	}
+
+	render() {
+
+		return(
+			<div>
+				This is the client
+			</div>
+		)
+
+	}
+
+}
---
+++ resources/ui/r63b/states/loading/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+import './loading.styl'
+
+export default class Loading extends Component {
+	 constructor(props) {
+		 super(props)
+
+		 this.state = {
+			 percent: 0
+		 }
+	 }
+
+	 componentDidMount() {
+		 this.interval = setInterval(() => {
+			 if(this.state.percent > 99) {
+				 this.setState({
+					 percent: 0
+				 })
+			 } else {
+				 this.setState({
+					 percent: this.state.percent + 10
+				 })
+			 }
+		 }, 500)
+	 }
+
+	 componentWillUnmount() {
+		 clearInterval(this.interval)
+	 }
+
+	 render() {
+
+		 return(
+			 <div className='loading'>
+			 	<div className='splash'>
+					<div className='photo'></div>
+					<div className='frame'></div>
+				</div>
+			 	<div className='text'>Please wait while {this.props.hotelName} is loading</div>
+			 	<div className='progress'>
+					<div className='bar' style={{width: this.state.percent + '%' }}>
+					</div>
+				</div>
+				<div className='percent'>{this.state.percent}%</div>
+			</div>
+		)
+	 }
+}
