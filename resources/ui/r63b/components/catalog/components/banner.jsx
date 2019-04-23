f53ea277fefbfc27d0af431887c127255c6f3577
Chiru B <chirub@foretag.co>
2019-04-23 23:04:06 +0100

    Banner (minor) update

--- resources/ui/r63b/components/catalog/components/banner.jsx
+++ resources/ui/r63b/components/catalog/components/banner.jsx
@@ -5,14 +5,21 @@ export default class CatalogBanner extends Component {
 		super(props)
 		
 		this.state = {}
+		
+		this.imageStyle = {
+			background: 'linear-gradient(rgba(14, 63, 82, 0.9), rgba(14, 63, 82, 0.9)), url(https://imgur.com/O1EqujY.gif)',
+			backgroundSize: '115%',
+			backgroundRepeat: 'no-repeat',
+			backgroundPosition: 'center'
+		}
 	}
 
 	render() {
 		return (
-			<div className='banner'>
+			<div className='banner' style={this.imageStyle}>
 				<h1 className='name'>{this.props.title}</h1>
 				<div className='image'>
-					<img src={this.props.image} width='200%' height='200%' />
+					<span className='overlay'></span>
 				</div>
 			</div>
 		)
