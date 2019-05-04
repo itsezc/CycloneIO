d42a381e7ea7c5d73d97cc0c5b766a0969a3b9cf
Chiru B <chirub@foretag.co>
2019-05-04 07:34:44 +0100

    Action buttons 0.0.2

--- resources/ui/r63b/components/actions/index.js
+++ resources/ui/r63b/components/actions/index.js
@@ -4,12 +4,22 @@ import { fullScreen } from '../../utils/class.functions'
 export default class Actions extends Component {
 	constructor(props) {
 		super(props)
+
+		this.state = {
+			fullScreen: false
+		}
 	}
 
+	toggleFullscreen = event => {
+		fullScreen(this.state.fullScreen)
+		this.state.fullScreen = this.state.fullScreen ? this.setState({ fullScreen: false }) : this.setState({ fullScreen: true })
+	}
+ 
 	render() {
 		return (
 			<div className='actions'>
-				<button className='action'>FS</button>
+				<button className='action'>WEB</button>
+				<button className='action' onClick={this.toggleFullscreen}>FS</button>
 			</div>
 		)
 	}
