ab50fe09beda7ccdfbe1b2d25ddaca3fc04ff07f
Thomas <dbmagouille@gmail.com>
2019-05-04 11:13:56 +0200

    Actions
    Actions redesign 1:1

--- resources/ui/r63b/components/actions/index.js
+++ resources/ui/r63b/components/actions/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { fullScreen } from '../../utils/class.functions'
 
 export default class Actions extends Component {
+	
 	constructor(props) {
 		super(props)
 
@@ -10,7 +11,7 @@ export default class Actions extends Component {
 		}
 	}
 
-	toggleFullscreen = event => {
+	toggleFullscreen = (event) => {
 		fullScreen(this.state.fullScreen)
 		this.state.fullScreen = this.state.fullScreen ? this.setState({ fullScreen: false }) : this.setState({ fullScreen: true })
 	}
@@ -18,8 +19,15 @@ export default class Actions extends Component {
 	render() {
 		return (
 			<div className='actions'>
-				<button className='arrow action'>WEB</button>
-				<button className='action' onClick={this.toggleFullscreen}>FS</button>
+
+				<button className='action-item action-item-arrow'>
+					<i className="icon icon-habbo"></i>
+					<span className="action-item-expand">WEB</span>
+				</button>
+
+				<button className='action-item' onClick={this.toggleFullscreen}>
+					<i className="icon icon-fs"></i>
+				</button>
 			</div>
 		)
 	}
