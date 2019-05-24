cf2bfa748b48b7e4dbce0fbc1632aa6aa41b24d9
Chiru B <chirub@foretag.co>
2019-05-24 17:55:11 +0100

    RoomQuiz states

--- resources/ui/r63b/components/poll/poll.jsx
+++ resources/ui/r63b/components/poll/poll.jsx
@@ -6,24 +6,46 @@ export default class Poll extends Component {
 
 		this.question = this.props.question || 'How is your day going?'
 		this.state = {
-			ended: true,
+			closed: props.status,
+			voted: false,
 			votes: {
 				negative: 0,
 				positive: 0
 			}
 		}
+
+		this.castPositiveVote = this.castPositiveVote.bind(this)
+		this.castNegativeVote = this.castNegativeVote.bind(this)
+	}
+
+	castNegativeVote() {
+		this.setState({
+			voted: true,
+			votes: {
+				negative: this.state.votes.negative + 1
+			}
+		})
+	}
+
+	castPositiveVote() {
+		this.setState({
+			voted: true,
+			votes: {
+				positive: this.state.votes.positive + 1
+			}
+		})
 	}
 
 	render() {
-		if(!this.state.ended) {
+		if(!this.state.voted) {
 			return(
 				<div className='poll'>
 					<h1 className='question'>{this.question}</h1>
 					<div className='options'>
-						<span className='option negative'>
-							<img src='/poll/thumbs_down.png' />
+						<span className='option negative' onClick={this.castNegativeVote}>
+							<img src='/poll/thumbs_down.png'  />
 						</span>
-						<span className='option positive'>
+						<span className='option positive' onClick={this.castPositiveVote}>
 							<img src='/poll/thumbs_up.png' />
 						</span>
 					</div>
@@ -34,11 +56,11 @@ export default class Poll extends Component {
 				<div className='poll'>
 					<div className='results'>
 						<span className='negative result'>
-							<p>5</p>
+							<p>{this.state.votes.negative || '0'}</p>
 						</span>
 						<h1 className='question'>{this.question}</h1>
 						<span className='positive result'>
-							<p>10</p>
+							<p>{this.state.votes.positive || '0'}</p>
 						</span>
 					</div>
 				</div>
