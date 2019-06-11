6df7b27a4c2a28ad59ff4882d3a05dcc4bba934b
Chiru B <chirub@foretag.co>
2019-06-11 22:38:56 +0100

    Mobile updates 0.0.4

--- source/ui/mobile/states/client/index.tsx
+++ source/ui/mobile/states/client/index.tsx
@@ -8,6 +8,21 @@ export default class Client extends Component {
 			<div className='client'>
 				<div className='room' id='game'>
 					<img className='icon' src='https://upload.wikimedia.org/wikipedia/commons/8/80/SemihReiz.png' />
+
+					<div className='navigation'>
+						<img className='avatar' src='https://www.habbo.com/habbo-imaging/avatarimage?hb=image&user=EZ-C&headonly=1&direction=2&head_direction=3&action=&gesture=&size=l' />
+					
+						<div className='news'></div>
+						<div className='navigator'></div>
+
+						<div className='center'>
+							<input type='text' placeholder='Say hi!' className='chatbar' />
+						</div>
+
+						<div className='store'></div>
+						<div className='inventory'></div>
+						<div className='friends'></div>
+					</div>
 				</div>
 			</div>
 		)
