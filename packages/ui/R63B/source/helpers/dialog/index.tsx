2176789572a2945f953a2cdb33f6bbda2827a70a
EZ C <chirub@foretag.co>
2019-07-17 18:59:11 +0100

    [UI][R63B] Catalog Featured
    Perfected featured images transformations and zoom

--- packages/ui/R63B/source/helpers/dialog/index.tsx
+++ packages/ui/R63B/source/helpers/dialog/index.tsx
@@ -189,13 +189,13 @@ export default class Dialog extends Component<DialogProps, any> {
 						<span>{this.props.title}</span>
                         {/* { this.closeable ? this.closeButton() : false }  */}
 
-                        <button className="btn btn-r63b btn-action btn-red">
+                        {/* <button className="btn btn-r63b btn-action btn-red">
                             <i className="icon icon-close"></i>
                         </button>
 
                         <button className="btn btn-r63b btn-action btn-blue">
                             <i className="icon icon-help"></i>
-                        </button>
+                        </button> */}
 					</div>
 
 					<div className='dialog-body'>
