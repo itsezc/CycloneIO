6d177689bc62fa3935cf33bcae838cd8432248b6
EZ C <chirub@foretag.co>
2019-08-07 03:29:15 +0100

    [Server] Chat Room Handler
    Added Chat function for Room Handler

--- packages/server/source/hotel/rooms/RoomManager.ts
+++ packages/server/source/hotel/rooms/RoomManager.ts
@@ -1,10 +1,15 @@
 export type RoomWithPlayers = {
     roomData: any,
     players: PlayerInfo[]
+    chat?: Message[]
 }
 
 import { prisma } from '../../../../storage/prisma'
 
+export type Message = {
+    from: string,
+    body: string
+}
 
 export type PlayerInfo = {
     socketId: string;
@@ -23,7 +28,7 @@ export class RoomManager {
         return RoomManager.instance
     }
 
-    private constructor(){
+    private constructor() {
         this.loadedRooms = []
     }
 
@@ -32,7 +37,7 @@ export class RoomManager {
         let roomWithPlayers: RoomWithPlayers = this.findRoom(id)
         console.log(roomWithPlayers)
 
-        if(!roomWithPlayers){
+        if(!roomWithPlayers) {
             let roomData = await prisma.room({
                 id
             })
@@ -70,6 +75,18 @@ export class RoomManager {
         }
 
         roomWithPlayers.players.push(playerInfo)
+        return true
+    }
+
+    public addChat(socketId: string, roomId: string): boolean {
+        let room: RoomWithPlayers = this.findRoom(roomId)
+
+        if(!room) {
+            return false
+        } else {
+            console.log('Chat event fired')
+        }
+
         return true
     }
 
