ea7c040741158585fda2192bd9bc01476321f055
EZ-C <33750251+itsezc@users.noreply.github.com>
2019-08-14 11:42:48 +0100

    [Client] Engine #1

---
+++ packages/client/core/engine.ts
@@ -0,0 +1,20 @@
+import Room from '../rooms/room'
+
+import ChatImager from '../chat/'
+import SoundManager from '../sound/manager'
+
+
+export default class Engine {
+
+	currentRoom?: Room
+	
+	chatImager: ChatImager
+	soundManager: SoundManager
+	
+	constructor() {
+
+		this.chatImager = new ChatImager()
+
+	}
+
+}
