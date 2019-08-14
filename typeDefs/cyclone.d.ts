2dc0343c2c78a527d777cb36c3f58554321ca936
EZ-C <33750251+itsezc@users.noreply.github.com>
2019-08-14 11:35:09 +0100

    typeDefs - Direction

---
+++ typeDefs/cyclone.d.ts
@@ -0,0 +1,13 @@
+/**
+ *  Directions
+ * 	0 = North 
+ *  1 = North East
+ *  2 = East 
+ *  3 = South East
+ *  4 = South
+ *  5 = South West 
+ *  6 = West
+ *  7 = North West
+ */
+
+export type Direction = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7
