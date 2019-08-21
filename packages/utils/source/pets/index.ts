40eeaf7a35c7c3859169a484df015ca111080e45
LevitationX <jasonolimpio3@gmail.com>
2019-08-21 23:42:11 +0100

    [Utils] Further data processing

--- packages/utils/source/pets/index.ts
+++ packages/utils/source/pets/index.ts
@@ -1,7 +1,15 @@
+import Path from 'path'
+
 import PetDownloader from './downloader'
 import PetConverter from './converter'
 
 import Logger from '../logger'
+
+export const ABSOLUTE_PATH = Path.join(__dirname, '..', '..', '..', '..', 'web-gallery', 'pets')
+
+enum Messages {
+    No_New_Data = 'Everything is up-to-date!'
+}
 
 class PetUtility {
     private readonly downloader: PetDownloader
@@ -15,17 +23,10 @@ class PetUtility {
     }
 
     private execute(): void {
-        let downloads = this.downloader.getDownloads()
 
-        const { downloadPromises, downloadData } = downloads
+        this.downloader.getDownloads().then(datas => {
 
-        downloadPromises.then(promises => {
-            if (promises.length === 0) {
-                Logger.info('Everything is up-to-date! :)')
-                process.exit(0)
-            }
-
-            downloadData.forEach(async (data) => {
+            datas.forEach(async (data) => {
                 const { petType, rawData } = data
 
                 const swf = await this.converter.extractSWF(rawData)
@@ -34,16 +35,39 @@ class PetUtility {
 
                 const images = await this.converter.extractImages(tags)
 
-                this.converter.writeImages(petType, images).then(imgesExists => {
-                    imgesExists || Logger.info('Images -> DONE')
+                var processPromises: Promise<any>[] = []
+                var fulfilledProcesses: boolean[] = []
+
+                let imagesPromise = this.converter.writeImages(petType, images).then(imagesExists => {
+                    if (!imagesExists) {
+                        Logger.info(`Images -> DONE [${petType}]`)
+                        fulfilledProcesses.push(imagesExists)
+                    }
                 })
 
-                this.converter.writeMetaData(petType).then(metaDataExists => {
-                    metaDataExists || Logger.info('Metadata -> DONE')
+                processPromises.push(imagesPromise)
+
+                let metaDataPromise = this.converter.writeMetaData(petType).then(metaDataExists => {
+                    if (!metaDataExists) {
+                        Logger.info(`Metadata -> DONE [${petType}]`)
+                        fulfilledProcesses.push(metaDataExists)
+                    }
                 })
 
-                this.converter.generateSpritesheet(petType).then(spritesheetGenerated => {
-                    !spritesheetGenerated || Logger.info('Spritesheet -> DONE')
+                processPromises.push(metaDataPromise)
+
+                let spritesheetPromise = this.converter.generateSpritesheet(petType).then(spritesheetGenerated => {
+                    if (spritesheetGenerated) {
+                        Logger.info(`Spritesheet -> DONE [${petType}]`)
+                        fulfilledProcesses.push(spritesheetGenerated)
+                    }
+                })
+
+                processPromises.push(spritesheetPromise)
+
+                Promise.all(processPromises).then(() => {
+                    fulfilledProcesses.length === 0 ? Logger.info(`${Messages.No_New_Data} [${petType}]`) :
+                        Logger.info(`Downloaded new data! [${petType}]`)
                 })
             })
         })
