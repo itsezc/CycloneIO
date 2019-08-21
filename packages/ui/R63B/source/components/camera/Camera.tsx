81dd1eb947aa0ba2c6c7af9b01e23e96e0ebdc98
EZ-C <33750251+itsezc@users.noreply.github.com>
2019-08-21 16:42:42 +0100

    [UI][R63B] Camera preview and state

--- packages/ui/R63B/source/components/camera/Camera.tsx
+++ packages/ui/R63B/source/components/camera/Camera.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Drag from 'react-drag-element'
+import { dragElement } from '../../utils/functions'
 
 import './Camera.styl'
 
@@ -9,42 +9,57 @@ export default class Camera extends Component<any, any> {
 		super(props)
 	
 		this.state = {
-			isOpen: true
+			isOpen: true,
+			maxPhotos: 5,
+			currentPhotos: 0,
+			isPreview: false
 		}		
+	}
+
+	public componentDidMount() {
+		dragElement(document.getElementById('cameraContainer'))
 	}
 
 	public close() {
 		this.setState({ isOpen: false })
-	}
-
-	public getFloatingComponentPosition = (positions: any) => {
-		console.log("FLOATING COMPONENT POSITIONS....", positions);
 	}
 	
 	public render() {
 
 		if (this.state.isOpen) {
 			return(
-				<Drag
-					dragItemId={'cameraContainer'}
-					dragId={'camera'}
-				>
-					<div className='cameraContainer' id='cameraContainer'>
-						<div className='camera' id='camera'>
-							<h1 className='title'>Habbo Camera</h1>
-		
-							<div className='cameraActions'>
-								<button className='btn btn-r63b btn-action btn-red' onClick={this.close.bind(this)}>
-									<i className='icon icon-close'></i>
-								</button>
-							</div>
-							
-		
-							<span className='hud'></span>
-							<span className='snap'></span>
+				<div className='cameraContainer' id='cameraContainer'>
+					<div className='camera' id='camera'>
+						<h1 className='title'>Habbo Camera</h1>
+	
+						<div className='cameraActions'>
+							<button className='btn btn-r63b btn-action btn-red' onClick={this.close.bind(this)}>
+								<i className='icon icon-close'></i>
+							</button>
+						</div>
+						
+						{ this.state.isPreview ? null : <span className='hud'></span> }
+
+						<span className='snap'></span>
+					</div>
+					<div className='gallery'>
+						<div className='imageContainer active'>
+							<img />
+						</div>
+						<div className='imageContainer'>
+							<img />
+						</div>
+						<div className='imageContainer'>
+							<img />
+						</div>
+						<div className='imageContainer'>
+							<img />
+						</div>
+						<div className='imageContainer'>
+							<img />
 						</div>
 					</div>
-				</Drag>
+				</div>
 			)
 		} else {
 			return null
