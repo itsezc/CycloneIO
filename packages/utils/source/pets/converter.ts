b15370124a91ffaad248f3fb5f559d344a2afc61
LevitationX <jasonolimpio3@gmail.com>
2019-08-21 18:03:52 +0100

    [Utils] Cyclone custom pet generator

--- packages/utils/source/pets/converter.ts
+++ packages/utils/source/pets/converter.ts
@@ -1,5 +1,114 @@
+import { readFromBufferP, extractImages, SWF, Tag, Image } from 'swf-extract'
+
+import FileSystem from 'fs'
+
+import Path from 'path'
+
+import Spritesmith, { Img } from 'spritesmith'
+
+const ABSOLUTE_PATH = Path.join(__dirname, '..', '..', '..', '..', 'web-gallery', 'pets')
+
+type MetaData = {
+    code: number,
+    characterId: number,
+    imgType: string,
+    imgName: string
+}
+
 export default class PetConverter {
+    private readonly metaData: MetaData[]
+    private readonly imgsSources: Map<string, string[]>
+
     public constructor() {
-        
+        this.metaData = []
+        this.imgsSources = new Map<string, string[]>() // <pet name, images paths>
+    }
+
+    public extractSWF(rawData: Buffer): SWF {
+        return readFromBufferP(rawData)
+    }
+
+    public async extractImages(tags: Tag[]): Promise<Image[]> {
+        return Promise.all(extractImages(tags))
+    }
+
+    public writeImages(petType: string, images: Image[]): boolean {
+        var newImgs: Buffer[] = []
+        var imgsPaths: string[] = []
+
+        images.map(image => {
+            const { code, characterId, imgType, imgData } = image
+            const imgName = `${petType}_${characterId}.${imgType}`
+
+            this.metaData.push({ code, characterId, imgType, imgName })
+
+            const imgsPath = Path.join(ABSOLUTE_PATH, petType, 'images')
+            const imgPath = Path.join(imgsPath, imgName)
+
+            imgsPaths.push(imgPath)
+
+            if (!FileSystem.existsSync(imgsPath)) {
+                FileSystem.mkdirSync(imgsPath)
+            }
+
+            if (!FileSystem.existsSync(imgPath)) {
+                FileSystem.writeFileSync(imgPath, imgData)
+                newImgs.push(imgData)
+            }
+        })
+
+        if (newImgs.length !== 0) {
+            return false
+        }
+
+        this.imgsSources.set(petType, imgsPaths)
+
+        return true
+    }
+
+    public writeMetaData(petType: string): boolean {
+        const imgsPath = Path.join(ABSOLUTE_PATH, petType, 'images')
+        const metaDataPath = Path.join(imgsPath, 'metadata.json')
+
+        if (!FileSystem.existsSync(imgsPath)) {
+            FileSystem.mkdirSync(imgsPath)
+        }
+
+        if (FileSystem.existsSync(metaDataPath)) {
+            return true
+        }
+
+        FileSystem.writeFileSync(metaDataPath, JSON.stringify(this.metaData, null, 2))
+
+        return false
+    }
+
+    public generateSpritesheet(petType: string): boolean {
+
+        let imgsSources = this.imgsSources.get(petType)
+
+        const spritesheetPath = Path.join(ABSOLUTE_PATH, petType)
+
+        // Spritesmith.run({ src: imgsSources }, (err: string, result: Result) => {
+        //     if (err) {
+        //         throw err
+        //     }
+
+        //     result.image // Buffer representation of image
+        //     result.coordinates // Object mapping filename to {x, y, width, height} of image
+        //     result.properties
+        // })
+
+        const spritesmith = new Spritesmith()
+
+        spritesmith.createImages(imgsSources, (err: string, images: Img[]) => {
+            if (err) {
+                throw err
+            }
+
+            console.log(images[0].height)
+        })
+
+        return false
     }
 }
--- packages/utils/source/pets/index.ts
+++ packages/utils/source/pets/index.ts
@@ -1,6 +1,7 @@
 import PetDownloader from './downloader'
 import PetConverter from './converter'
-import Logger from '../logger';
+
+import Logger from '../logger'
 
 class PetUtility {
     private readonly downloader: PetDownloader
@@ -10,19 +11,47 @@ class PetUtility {
         this.downloader = new PetDownloader()
         this.converter = new PetConverter()
 
-        this.initialize()
+        this.execute()
     }
 
-    private initialize(): void {
+    private execute(): void {
         let downloads = this.downloader.getDownloads()
 
-        downloads.downloadPromises.then(promises => {
+        const { downloadPromises, downloadData } = downloads
+
+        downloadPromises.then(promises => {
             if (promises.length === 0) {
                 Logger.info('Everything is up-to-date! :)')
                 process.exit(0)
             }
 
-            console.log(downloads.downloadData)
+            downloadData.forEach(async (data) => {
+                const { petType, rawData } = data
+
+                const swf = await this.converter.extractSWF(rawData)
+
+                const { tags } = swf
+
+                const images = await this.converter.extractImages(tags)
+
+                let imagesExists = this.converter.writeImages(petType, images)
+
+                if (!imagesExists) {
+                    Logger.info('Images -> DONE')
+                }
+
+                let metaDataExists = this.converter.writeMetaData(petType)
+
+                if (!metaDataExists) {
+                    Logger.info('Metadata -> DONE')
+                }
+
+                let spritesheetGenerated = this.converter.generateSpritesheet(petType)
+
+                if (spritesheetGenerated) {
+                    Logger.info('Spritesheet -> DONE')
+                }
+            })
         })
     }
 }
