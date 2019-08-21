8e3394ebdbd927865130194ba027eb014ccb332d
LevitationX <jasonolimpio3@gmail.com>
2019-08-21 19:23:06 +0100

    [Utils] Type definitions

--- types/spritesmith.d.ts
+++ types/spritesmith.d.ts
@@ -24,21 +24,6 @@ type Properties = {
     height: number
 }
 
-export type Img = {
-    data: Uint8Array[]
-    shape: number[],
-    stride: number[],
-    offset: number
-    width: number,
-    height: number,
-    filepath: string
-}
-
 declare module 'spritesmith' {
     export function run(config: Config, handleResult: (err: string, result: Result) => void): void
-
-    export default class Spritesmith {
-        public constructor()
-        public createImages(src: string[], handleImages: (err: string, images: Img[]) => void): void
-    }
 }
