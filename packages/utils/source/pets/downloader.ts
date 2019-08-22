9fcd1588ee3c70d430d89b4342d62c352073a9e6
LevitationX <jasonolimpio3@gmail.com>
2019-08-22 17:31:52 +0100

    [Utils] Deleted useless messages

--- packages/utils/source/pets/downloader.ts
+++ packages/utils/source/pets/downloader.ts
@@ -33,8 +33,6 @@ export default class PetDownloader {
                 FileSystem.mkdirSync(ABSOLUTE_PATH)
             }
 
-            Logger.info('Downloading new assets...')
-
             var downloads: Promise<Buffer>[] = []
 
             petAssets.forEach(petAsset => {
